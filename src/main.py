77b4e77a300310b9401a082747a1bc30c7468ae6
stevezhu <steveju546@gmail.com>
2022-11-12 19:52:51 -0600

    This is the 6th commit for stock.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
 import robin_connection as rc
+
+
 class main:
-    steve_rc = rc.robin_connection('Steve')
-    steve_rc.robin_login('636396')
+    try:
+        steve_rc = rc.robin_connection('Steve')
+        steve_rc.robin_login('636396')
+        print('Steve has logged in.')
+    except:
+        print('Login failed')
+        print(Exception)
+
     profile = steve_rc.see_full_profile()
     # print(profile)
     profile = steve_rc.see_simple_profile()
