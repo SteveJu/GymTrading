9abd7bcb53c46588eedf5a37c5c4f6536e922c25
Steve Ju <steveju546@gmail.com>
2022-12-07 00:05:02 -0600

    This commit is for log functions.

--- src/simulator.py
+++ src/simulator.py
@@ -1,18 +1,23 @@
 import collections
 
+float_list = ['Total Asset', 'Strike', 'Price per share', 'Total earning']
+int_list = ['Shares']
+
 
 def readCurrent():
     portfolio = collections.defaultdict()
-    with open('log.txt', 'r') as f:
+    with open('logs/portfolio.txt', 'r') as f:
         lines = f.readlines()
-    curr = lines[-1].split(';')
+    curr = lines[-1].split('; ')
     curr = curr[:-1]
     for item in curr:
         items = item.split(': ')
         key = items[0]
-        try:
+        if key in float_list:
             value = float(items[1])
-        except:
+        elif key in int_list:
+            value = int(items[1])
+        else:
             value = items[1]
         portfolio[key] = value
     return portfolio
@@ -22,6 +27,6 @@ def getCurrAsset(portfolio):
     return portfolio['Total Asset']
 
 
-def writeToLog(message):
-    with open('log.txt', 'w') as f:
+def writeBuy(message):
+    with open('logs/operations_log.txt', 'w') as f:
         f.write(message)
